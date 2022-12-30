54f0e4836ef634cd71b66b75d29987b7488c18c2
Talha Javed <talhachughtai1122@gmail.com>
2022-12-30 15:57:16 +0500

    fixes

--- client/src/PortfolioContainer/Home/Profile/Profile.js
+++ client/src/PortfolioContainer/Home/Profile/Profile.js
@@ -9,19 +9,19 @@ export default function Profile() {
         <div className="profile-details">
           <div className="colz">
             <div className="cols-icon">
-              <a href="https://web.facebook.com/?_rdc=1&_rdr">
+              <a href="https://www.facebook.com/talha.chughtai.12/">
                 <i className="fa fa-facebook-square" />
               </a>
               <a href="#">
                 <i className="fa fa-google-plus-square" />
               </a>
-              <a href="https://www.instagram.com/instructor_ehizeex/">
+              <a href="https://www.instagram.com/talha.chughtai/">
                 <i className="fa fa-instagram" />
               </a>
-              <a href="https://www.youtube.com/channel/UCSSr5ZDFbilpZ592_ycoAwA">
+              <a href="https://www.youtube.com">
                 <i className="fa fa-youtube-square" />
               </a>
-              <a href="https://twitter.com/Ehiedu_baba">
+              <a href="https://twitter.com/TalhaCh82559507">
                 <i className="fa fa-twitter" />
               </a>
             </div>
@@ -58,7 +58,7 @@ export default function Profile() {
 
           <div className="profile-options">
             <button className="btn primary-btn"> Hire Me </button>
-            <a href="ehizcv.pdf" download="Ehiedu Ehizcv.pdf">
+            <a href="Talha-Resume.pdf" download="Talha-Resume.pdf">
               <button className="btn highlighted-btn">Get Resume</button>
             </a>
           </div>
